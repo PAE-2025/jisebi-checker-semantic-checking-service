bb7367efe04ff03d679a481c96f7b7712e3b79b3
Thaofoo <mthafixh@gmail.com>
2025-05-14 22:30:37 +0700

    fix: config and env

--- src/config.py
+++ src/config.py
@@ -1,17 +1,15 @@
-import json
-# Load konfigurasi dari file JSON
-with open("config.json") as con_file:
-    config = json.load(con_file)
-
 from pydantic_settings import BaseSettings
     
 class Settings(BaseSettings):
     APP_NAME: str = "Jisebi Checker"
     DEBUG_MODE: bool = True
-    SCOPUS_API_KEY = config["apikey"]
+    SCOPUS_API_KEY: str
     SPACY_MODEL: str = "en_core_web_sm"
     
     # API settings
     API_PREFIX: str = "/api/v1"
+
+    class Config:
+        env_file = ".env"
     
 settings = Settings()
