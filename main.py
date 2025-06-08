c2ada6e44f524dc4384201e4dff3d0e3dc19f941
Thaofoo <mthafixh@gmail.com>
2025-06-09 06:56:50 +0700

    chore: standardize routing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import search
+from src.novelty_checker.router import router as novelty_router
 from src.discon_analyzer.router import router as discon_analyzer_router
 from src.ner.router import router as ner_router
 from src.grammar_checking.router import router as grammar_router
@@ -8,7 +8,7 @@ from src.abstract_checking.router import router as abstract_router
 app = FastAPI(title="Journal Search API", description="Find similar journals using Scopus API")
 
 # Register Routers
-app.include_router(search.router, prefix="/api/novelty", tags=["Search"])
+app.include_router(novelty_router.router, prefix="/api/novelty", tags=["Search"])
 app.include_router(discon_analyzer_router, prefix="/api/discon", tags=["Discon"])
 app.include_router(ner_router, prefix="/api/ner", tags=["NER"])
 app.include_router(grammar_router, prefix="/api/grammar", tags=["Grammar"])
