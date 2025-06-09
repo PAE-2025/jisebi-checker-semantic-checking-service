52d0b9104eeaf57acd26eff60e4b2bb580035021
Thaofoo <mthafixh@gmail.com>
2025-06-09 08:32:22 +0700

    revise: routing error

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from src.abstract_checking.router import router as abstract_router
 app = FastAPI(title="Journal Search API", description="Find similar journals using Scopus API")
 
 # Register Routers
-app.include_router(novelty_router.router, prefix="/api/novelty", tags=["Search"])
+app.include_router(novelty_router, prefix="/api/novelty", tags=["Search"])
 app.include_router(discon_analyzer_router, prefix="/api/discon", tags=["Discon"])
 app.include_router(ner_router, prefix="/api/ner", tags=["NER"])
 app.include_router(grammar_router, prefix="/api/grammar", tags=["Grammar"])
